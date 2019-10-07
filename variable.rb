7de635193bfd6cc9ea2aff8722b909651e675218
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-07 13:28:35 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,15 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+#def greeting(greeting_phrase)
+ # puts greeting_phrase
+#end #greeting
+
+#def say_greeting_five_times
+#  5.times do
+ #   greeting(greeting_phrase)
+ # end
+#end
+
+#greeting_phrase = "Vanakkam"
+#say_greeting_five_times
+greeting = "Hello World"
